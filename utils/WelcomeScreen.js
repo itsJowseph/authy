f02d30f3c51e99d86aaf79e08d835d23c021b167
itsJowseph <2201159@ub.edu.ph>
2024-11-25 16:23:32 +0800

    first commit

---
+++ utils/WelcomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+
+const WelcomeScreen = ({ route, navigation }) => {
+  const { username } = route.params;
+
+
+  return (
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontSize: 24, marginBottom: 20 }}>Welcome back, {username}!</Text>
+      <Button title="Log Out" onPress={() => navigation.navigate('Login')} />
+    </View>
+  );
+};
+
+
+export default WelcomeScreen;
